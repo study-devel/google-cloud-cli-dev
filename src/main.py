d651872399538665b1003b8df7030bb8301d28b2
ry-yamafuji <ry.yamafuji@gmail.com>
2024-02-06 06:11:55 +0900

    CloudFunctionをデプロイ修正2

--- src/main.py
+++ src/main.py
@@ -3,4 +3,12 @@ def hello_world(request):
     HTTP Cloud Function.
     request (flask.Request): The request object.
     """
-    return "Hello, World! Auto Deploy", 200
+    return "Hello, World!", 200
+
+
+def hello_world_deploy(request):
+    """
+    HTTP Cloud Function.
+    request (flask.Request): The request object.
+    """
+    return "Hello, World! Git Action", 200
