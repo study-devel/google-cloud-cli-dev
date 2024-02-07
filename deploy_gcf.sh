a3094d87a00fc69af06ecf955b2708f1eee69a8b
ry-yamafuji <ry.yamafuji@gmail.com>
2024-02-07 21:08:25 +0900

    デプロイ修正

---
+++ deploy_gcf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Google Cloud のプロジェクトIDを設定
+PROJECT_ID="gcp-devel-project"
+
+# デプロイする関数の名前
+FUNCTION_NAME="hello_world"
+# 使用するランタイム（例: python39, nodejs12 など）
+RUNTIME="python39"
+
+# トリガーのタイプ（例: --trigger-http, --trigger-topic, --trigger-event など）
+TRIGGER="--trigger-http"
+
+# プロジェクトIDをセット
+gcloud config set project $PROJECT_ID
+
+# 関数をデプロイ
+gcloud functions deploy $FUNCTION_NAME \
+    --runtime $RUNTIME $TRIGGER \
+    --allow-unauthenticated \
+    --region=asia-northeast1 \
+    --source=./src
+
+echo "Function deployed successfully"
--- src/main.py
+++ src/main.py
@@ -6,9 +6,3 @@ def hello_world(request):
     return "Hello, World!", 200
 
 
-def hello_world_deploy(request):
-    """
-    HTTP Cloud Function.
-    request (flask.Request): The request object.
-    """
-    return "Hello, World! Git Action", 200
